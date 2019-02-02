e79f961804427ade2e79d02528ed75ea0ec34a9c
cberry216 <christopherberry216@gmail.com>
2019-02-02 17:33:01 -0500

    Created HTML templates and added pagination to site.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 
 # Create your views here.
 
 
 def post_list(request):                               # Request required for all views
-    posts = Post.published(all)                       # Get all published posts
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 1)             # Only 1 post per page
+    page = request.GET.get('page')                    # Current page (integer)
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:                          # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:                                 # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+
     return render(request,
                   'blog/post/list.html',
-                  {'posts': posts})                   # Context variables to be used within the page
+                  {'page': page,
+                   'posts': posts})                   # Context variables to be used within the page
 
 
 def post_detail(request, year, month, day, post):
