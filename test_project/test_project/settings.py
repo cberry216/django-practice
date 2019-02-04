e3a4b96acde6eb387acee2940c1acb375b6de4ed
cberry216 <christopherberry216@gmail.com>
2019-02-04 12:36:36 -0500

    Reorganized test_project into its own directory.

--- test_project/test_project/settings.py
+++ test_project/test_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ohfr3(j(-=h^xte0--@v(!1rumz+$0&p!gb_doczcx=((o6)yv'
+SECRET_KEY = 'w($$i2kfrf3rn)#rs(#ee_28len(&$sx0%4$h*f0!bcrs15z)k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
