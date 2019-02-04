dbf37ee2e47b6dd5d7d2c9acb893c2ace0012322
cberry216 <christopherberry216@gmail.com>
2019-02-04 13:26:01 -0500

    Created simple hello world app using views and URLConf.

--- helloworld_project/helloworld_project/settings.py
+++ helloworld_project/helloworld_project/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # New apps go at the top
+    'pages.apps.PagesConfig',
+    # Built-in apps go below
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
