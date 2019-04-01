bd4a5ced6999a62781a80c7d1a7282464336538b
cberry216 <christopherberry216@gmail.com>
2019-04-01 09:28:45 -0400

    Switched login to GCBV and add logout URLConfs

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView
+)
+
 from .views import (
     user_login,
 )
@@ -8,5 +13,7 @@ app_name = 'account'
 
 urlpatterns = [
     # Post Views
-    path('login/', user_login, name='login'),
+    # path('login/', user_login, name='login'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
