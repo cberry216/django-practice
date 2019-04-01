c62d229dfe858519d2ca0ed7ee891554aabb3017
cberry216 <christopherberry216@gmail.com>
2019-04-01 09:47:03 -0400

    Added dashboard URLConf to account.

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -5,9 +5,14 @@ from django.contrib.auth.views import (
     LogoutView
 )
 
+from .views import (
+    dashboard
+)
+
 app_name = 'account'
 
 urlpatterns = [
+    path('', dashboard, name='dashboard'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
