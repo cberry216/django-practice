7932b26e5ff2fc1ba3726f57675cc8589d8a32ae
cberry216 <christopherberry216@gmail.com>
2019-04-03 15:52:32 -0400

    Added media root and url to project settings.

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -130,3 +130,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
