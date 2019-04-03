9829d955d4680d02703b3795d46f91f44203cb5a
cberry216 <christopherberry216@gmail.com>
2019-04-03 09:10:18 -0400

    Added password change and done URLConfs.

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
+from django.contrib.auth.views import (
+    PasswordChangeView,
+    PasswordChangeDoneView,
+)
 
 from django.contrib.auth.views import (
     LoginView,
@@ -15,4 +19,14 @@ urlpatterns = [
     path('', dashboard, name='dashboard'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path(
+        'password_change/',
+        PasswordChangeView.as_view(),
+        name='password_change'
+    ),
+    path(
+        'password_change/done/',
+        PasswordChangeDoneView.as_view(),
+        name='password_change_done'
+    )
 ]
